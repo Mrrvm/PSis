c6c65eb8d9da2396ddc6a3c899bb208db42cf8e3
Mrrvm <mrrvm@hotmail.com>
2017-02-25 13:05:20 +0000

    Added my I assignment version

---
+++ Labs/lab1/I/concatenation_mrrvm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *concatenate(char dest[], char const src[]) {
+	int i = 0, j = 0;
+	i = strlen(dest);
+	for(j = 0; src[j] != '\0'; j++, i++) {
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+}
+
+int main(int argc, char const *argv[]) {
+
+	size_t result_string_len = 1024;
+	int i = 0;
+	char *result_str = (char *)malloc(result_string_len);
+
+	for(i = 1; i < argc; i++) {
+
+		if(strlen(result_str)+strlen(argv[i])+1 <= result_string_len) {
+			concatenate(result_str, argv[i]);
+		}
+		else {
+			result_string_len += strlen(argv[i])+1024;
+			result_str = (char*) realloc(result_str, result_string_len);
+			concatenate(result_str, argv[i]);
+		}
+	}
+
+	printf("\nThe final string is: %s\n", result_str);
+
+	return 0;
+}
