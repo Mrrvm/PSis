f5bec4b0731b1206073f6d53450421bd7d44cc78
Mrrvm <mrrvm@hotmail.com>
2017-02-28 14:38:51 +0000

    V is done!

--- Labs/lab1/V/prog2.c
+++ Labs/lab1/V/prog2.c
@@ -1,23 +1,37 @@
-#include "test.h"
+#include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef void (*func)(void);
+
+int main() {
 	int a;
 	printf("What version of the functions you whant to use?\n");
 	printf("\t1 - Normal    (test1)\n");
 	printf("\t2 - Optimized (test2)\n");
 	scanf("%d", &a);
-	if (a == 1){
+	if (a == 1) {
 		/* load library test1 */
-
-		printf("running the normal versions from \n");
-	}else{
-		if(a== 2){
+		func func_1, func_2;
+		void *lib1 = dlopen("test1.so", RTLD_LAZY);
+		func_1 = dlsym(lib1, "func_1");
+		func_2 = dlsym(lib1, "func_2");
+		(*func_1)();
+		(*func_2)();	
+		dlclose(lib1);
+	}
+	else {
+		if(a== 2) {
 			/* load library test2 */
-
-			printf("running the normal versions\n");
-		}else{
+			func func_1, func_2;
+			void *lib2 = dlopen("test2.so", RTLD_LAZY);
+			func_1 = dlsym(lib2, "func_1");
+			func_2 = dlsym(lib2, "func_2");
+			(*func_1)();
+			(*func_2)();	
+			dlclose(lib2);
+		}
+		else {
 			printf("Not running anything\n");
 			exit(-1);
 		}
