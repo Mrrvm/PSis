0be4bed9ad3e55bf9b5fd27c3b66e52c836cd119
Firmino <joao_firmino28@hotmail.com>
2017-03-15 00:04:15 +0000

    Does 1 child at a time. Does not save time slept

---
+++ Labs/lab3_F/II.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	int i, pid;
+	int total_time_slept = 0;
+	int sleeptime = 0;
+
+
+
+
+	for(i = 0; i < 10; ++i)
+	{	
+
+		pid = fork();
+
+		if(pid == 0)
+		{	
+			srandom(time(NULL)^i);
+			printf("\n[Child %d] Fell asleep\n", getpid());
+
+			sleeptime = random()%10;
+
+			sleep(sleeptime);
+			printf("%d\n", sleeptime );
+			
+			printf("[Child %d] Woke up\n", getpid() );
+
+			exit(0);
+
+
+		}
+		else
+		{
+			pid = wait(NULL);
+			total_time_slept = total_time_slept + sleeptime;
+			printf("\n[Parent] Child %d slept for %d seconds\n", pid, sleeptime);
+		}
+	}
+
+	printf("[Parent] Total time slept: %d\n", total_time_slept );
+
+
+	return 0;
+}
