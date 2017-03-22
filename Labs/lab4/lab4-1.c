02d27471f9725558bea13a62e43392a7625bcd00
Firmino <joao_firmino28@hotmail.com>
2017-03-22 11:15:14 +0000

    working

--- Labs/lab4/lab4-1.c
+++ Labs/lab4/lab4-1.c
@@ -1,5 +1,11 @@
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
 
 typedef struct{
 		unsigned start, end;
@@ -10,21 +16,66 @@ int main(){
 	m_7 = m_19 = m_7_19 = 0;
 	com_struct limits;
 
-	limits.start = 0;
-	limits.end = UINT_MAX;
-	for (unsigned int  i = limits.start; i <  limits.end; i++){
-		if(i%7 == 0){ 
-			m_7 ++;
+	int j = 0;
+	int pid;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		pid = fork();
+
+		if (pid == 0)
+		{
+			if (j == 0)
+			{
+				limits.start = 0;
+				limits.end = UINT_MAX/3;
+			}
+			else if (j == 1)
+			{
+				limits.start = UINT_MAX/3 +1;
+				limits.end	= UINT_MAX*2/3;
+			}
+			else if (j == 2)
+			{
+				limits.start = UINT_MAX*2/3;
+				limits.end = UINT_MAX;
+			}
+
+			break;
 		}
-		if(i%19 == 0){ 
-			m_19 ++;
-		}
-		if((i%7 == 0)	&& (i%19 == 0)){
-			m_7_19++;
-		}
+
+		j++;
+
 	}
-	printf("m 7    %d\n", m_7);
-	printf("m   19 %d\n", m_19);
-	printf("m 7 19 %d\n", m_7_19);
+
+
+	if (pid == 0)
+	{
+		for (unsigned int  a = limits.start; a <  limits.end; a++)
+		{
+			if(a%7 == 0)
+				m_7 ++;
+			
+			if(a%19 == 0) 
+				m_19 ++;
+			
+			if((a%7 == 0)	&& (a%19 == 0))
+				m_7_19++;
+			
+		}
+
+		printf("m 7    %d\n", m_7);
+		printf("m   19 %d\n", m_19);
+		printf("m 7 19 %d\n\n", m_7_19);
+
+		exit(0);
+	}
+
+	for (int b = 0; b < 3; ++b)
+	{
+		wait(NULL);
+	}
+
+	return 0;
 
 }
