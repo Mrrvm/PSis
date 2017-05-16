8ac54223f16a3ad36b616c106dc7cb429ff59bcf
Mrrvm <mrrvm@hotmail.com>
2017-05-16 20:19:27 +0100

    still nthign

--- Project/lib/gallery_add_photo.c
+++ Project/lib/gallery_add_photo.c
@@ -9,22 +9,23 @@ uint32_t gallery_add_photo(int peer_socket, char *file_name) {
 	photo_data photo_data_;
 	FILE *photo;
 	size_t photo_size;
-	char *buffer;
 
 	photo = fopen(file_name, "rb");
 	fseek(photo, 0, SEEK_END);
 	photo_size = ftell(photo);
 	fseek(photo, 0, SEEK_SET);
-	buffer = malloc(photo_size);
+	char *buffer[photo_size];
 	
+	photo_size = htons(photo_size);
 	send(peer_socket, &photo_size, sizeof(photo_size), 0);
+	
 	while(!feof(photo)) {
 		fread(buffer, 1, sizeof(buffer), photo);
     	send(peer_socket, buffer, photo_size, 0);
     	bzero(buffer, sizeof(buffer));
 	}
 	
-	strcpy(photo_data_.file_name, file_name);
-	photo_data_.id_photo = 0;
-	send(peer_socket, &photo_data_, sizeof(photo_data_), 0);
+	// strcpy(photo_data_.file_name, file_name);
+	// photo_data_.id_photo = 0;
+	// send(peer_socket, &photo_data_, sizeof(photo_data), 0);
 }
--- Project/peers/handle_client.c
+++ Project/peers/handle_client.c
@@ -5,17 +5,17 @@ void *handle_client(void * arg) {
 	int client_socket = *(int *) arg;
 	int photo_size;
 	photo_data photo_data_;
-	char *buffer;
 	FILE *photo;
 
 	recv(client_socket, &photo_size, sizeof(photo_size), 0);
-
-	buffer = malloc(photo_size);
+	photo_size = ntohs(photo_size);
+	char *buffer[photo_size];
 	recv(client_socket, buffer, photo_size, 0);
 
-	photo = fopen("nude_received.jpg", "w");
+	photo = fopen("photos/nude_received.png", "w");
 	fwrite(buffer, 1, sizeof(buffer), photo);
 	fclose(photo);
 
-	recv(client_socket, photo_data_, sizeof(photo_data_), 0);
+	// recv(client_socket, &photo_data_, sizeof(photo_data), 0);
+	// printf("%s\n", photo_data_.file_name);
 }
