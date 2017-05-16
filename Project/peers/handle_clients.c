df6d94ef86d75c5817135f9419855d78fa2a21a3
Firmino <joao_firmino28@hotmail.com>
2017-05-16 19:42:38 +0100

    deleted conn_args

--- Project/peers/handle_clients.c
+++ Project/peers/handle_clients.c
@@ -7,8 +7,6 @@ void *handle_clients(void * arg) {
 
 	int sock_stream = *(int *) arg;
 	int sock_client;
-	socklen_t size_addr;
-	conn_args *new_conn_args;
 	int error;
 	pthread_t thr_client;
 
@@ -18,14 +16,11 @@ void *handle_clients(void * arg) {
 	while(1) {
         
         printf("Waiting connection from client...\n");
-        new_conn_args = malloc(sizeof(conn_args));
-        size_addr = sizeof(new_conn_args->client_addr_);
-        sock_client = accept(sock_stream, 
-                    (struct sockaddr *) &new_conn_args->client_addr_, 
-                    &size_addr);
-        new_conn_args->sock_client_ = sock_client; 
+
+        sock_client = accept(sock_stream, NULL, NULL);
+
         
-        error = pthread_create(&thr_client, NULL, handle_client, (void *)new_conn_args);
+        error = pthread_create(&thr_client, NULL, handle_client, &sock_client);
 		if(error != 0) {
 			perror("Unable to create thread to handle clients.");
 			exit(-1);
--- Project/peers/peer.h
+++ Project/peers/peer.h
@@ -3,8 +3,3 @@
 
 void *handle_clients(void * arg);
 void *handle_client(void * arg);
-
-typedef struct conn_args_ {
-	int sock_client_;
-	struct sockaddr_in client_addr_;
-} conn_args;
