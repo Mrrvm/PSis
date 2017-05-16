f65ebed705f59bd0c3e7cfb31bdfa25bcf98eec1
Mrrvm <mrrvm@hotmail.com>
2017-05-16 19:01:30 +0100

    added photo data structure

--- Project/lib/gallery_add_photo.c
+++ Project/lib/gallery_add_photo.c
@@ -2,9 +2,11 @@
 
 uint32_t gallery_add_photo(int peer_socket, char *file_name) {
 	
-	int test;
+	// int test = htons(14);
+	// printf("Adding %s\n", file_name);
+	// send(peer_socket, &test, sizeof(test), 0);
+	photo_data photo_data_;
 
-	test = htons(14);
-	printf("Adding %s\n", file_name);
-	send(peer_socket, &test, sizeof(test), 0);
+
+	send(peer_socket, &photo_data_, sizeof(photo_data_));
 }
