ede237c29d7131020cf0d0caabadeadf6541d7ff
Firmino <joao_firmino28@hotmail.com>
2017-05-18 00:24:15 +0100

    new sockets

--- Project/gateway/handle_peer_list.c
+++ Project/gateway/handle_peer_list.c
@@ -11,6 +11,7 @@ void *handle_peer_list(void * arg) {
     struct sockaddr_in local_addr;
     struct sockaddr_in peer_addr;
     int sock_local;
+    int sock_peer;
     list *servers_list = (list *)arg;
     peer_data *peer_data_;
 
@@ -28,6 +29,16 @@ void *handle_peer_list(void * arg) {
     while(1) {
 
         recv(sock_local, (struct sockaddr *) &peer_addr, sizeof(peer_addr), 0);
+
+        sock_peer = socket(AF_INET, SOCK_STREAM, 0);
+        local_addr.sin_family = AF_INET;
+        local_addr.sin_port= htons(3002);
+        inet_aton(argv[1], &local_addr.sin_addr);
+        bind(sock_peer, (struct sockaddr *)&local_addr, sizeof(local_addr));
+        listen(sock_peer, 10);
+        sock_peer = accept(sock_peer, NULL, NULL);
+
+
         peer_data_->peer_addr = peer_addr;
         push_item_to_list(servers_list, peer_data_);
         print_list(servers_list, print_server);
