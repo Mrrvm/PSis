ebd95aefeb5b1faaf8546a73c7ca89d03316756c
Mrrvm <mrrvm@hotmail.com>
2017-05-25 00:36:46 +0100

    Added functions todo

--- Project/client/lib.c
+++ Project/client/lib.c
@@ -86,3 +86,23 @@ uint32_t gallery_add_photo(int peer_socket, char *file_name) {
 	}
 	free(photo_data_);
 } 
+
+int gallery_add_keyword(int peer_socket, uint32_t id_photo, char *keyword) {
+
+}
+
+int gallery_search_photo(int peer_socket, char * keyword, uint32_t ** id_photos) {
+
+}
+
+int gallery_delete_photo(int peer_socket, uint32_t id_photo) {
+
+}
+
+int gallery_get_photo_name(int peer_socket, uint32_t id_photo, char **photo_name) {
+
+}
+
+int gallery_get_photo(int peer_socket, uint32_t id_photo, char *file_name) {
+	
+}
