e2b3e3d20de6e23e6d9ad4c2fbf6376e90ca13b0
Mrrvm <mrrvm@hotmail.com>
2017-05-26 19:28:41 +0100

    ..

--- Project/client/client0.c
+++ Project/client/client0.c
@@ -32,11 +32,8 @@ int main(int argc, char *argv[])
 		id_photo = gallery_add_photo(connect_ret, "photos/my_first_nude.jpg");
 		printf("Received id_photo: %d\n", id_photo);
 		//gallery_delete_photo(connect_ret, 1);
-		photo_name = malloc(sizeof(char *));
-		strcpy(photo_name, "nude_recv");
-		if(1 == gallery_get_photo(connect_ret, id_photo, photo_name)) 
-			printf("Received %s\n", photo_name);
-		free(photo_name);
+		if(1 == gallery_get_photo(connect_ret, id_photo, "nude_recv")) 
+			printf("Received photo\n");
 	}
 	return 0;
 }
