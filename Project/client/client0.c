fa5098f267d03722ccc9043a4f91963c13f46b68
Mrrvm <mrrvm@hotmail.com>
2017-06-02 19:53:10 +0100

    ended client

--- Project/client/client0.c
+++ Project/client/client0.c
@@ -6,12 +6,13 @@ int main(int argc, char *argv[])
 	in_port_t port = 0;
 	int connect_ret = 1;
 	int fcn_number = 0;
+	int ret = 0;
+	int i = 0;
 	uint32_t id_photo;
-	char photo_name[30];
-
 	uint32_t *id_photos[100] = {0};
+	char photo_name[30];
+	char keyword[30];
 	char *photo_name_ptr;
-	char *keyword;
 
 	// Checks if user inserted the write input
 	if(argc != 3) {
@@ -41,21 +42,42 @@ int main(int argc, char *argv[])
 				printf("[Insert file name]: ");
 				scanf("%s", photo_name);
 				// Adds a new photo to the gallery
-				id_photo = gallery_add_photo(connect_ret, photo_name);
-				printf("\nReceived id is: %d\n", id_photo);
+				ret = gallery_add_photo(connect_ret, photo_name);
+				printf("\nfcn: ADD PHOTO returns %d\n", ret);
 			}
 			else if(fcn_number == 2) {
 				printf("[Insert keyword]: ");
-				gallery_add_keyword(connect_ret, id_photo, "nude");
+				scanf("%s", keyword);
+				printf("[Insert id]: ");
+				scanf("%u", &id_photo);
+				ret = gallery_add_keyword(connect_ret, id_photo, keyword);
+				printf("\nfcn: ADD KEYWORD returns %d\n", ret);
 			}
 			else if(fcn_number == 3) {
-				
+				printf("[Insert keyword]: ");
+				scanf("%s", keyword);
+				ret = gallery_search_photo(connect_ret, keyword, id_photos);
+				printf("\nfcn: SEARCH PHOTO returns %d\n", ret);
+				printf("Found ids: ");
+				while(id_photos[i] != 0) {
+					printf("%d ", id_photos[i]);
+					i++;
+				}
+				printf("\n");
 			}
 			else if(fcn_number == 4) {
-				
+				printf("[Insert id]: ");
+				scanf("%u", &id_photo);
+				ret = gallery_delete_photo(connect_ret, id_photo);
+				printf("\nfcn: DELETE PHOTO returns %d\n", ret);
 			}
 			else if(fcn_number == 5) {
 				
+				printf("[Insert id]: ");
+				scanf("%u", &id_photo);
+				ret = gallery_get_photo_name(connect_ret, id_photo, &photo_name_ptr);
+				printf("\nfcn: GET PHOTO NAME returns %d\n", ret);
+				printf("Received name %s\n", photo_name_ptr);
 			}
 			else if(fcn_number == 6) {
 				
@@ -65,27 +87,6 @@ int main(int argc, char *argv[])
 				exit(-1);
 			}
 		}
-		printf("Received id_photo: %d\n", id_photo);
-
-		gallery_add_keyword(connect_ret, id_photo, "hot");
-
-		id_photo = gallery_add_photo(connect_ret, "photos/nude2.png");
-		printf("Received id_photo: %d\n", id_photo);
-		
-		gallery_add_keyword(connect_ret, id_photo, "trump");
-		gallery_add_keyword(connect_ret, id_photo, "hot");
-
-		gallery_search_photo(connect_ret, "hot", id_photos);
-		
-		
-		if(1 == gallery_get_photo(connect_ret, id_photo, "recv_nude"));
-			printf("Received photo\n");
-
-		if(1 == gallery_get_photo_name(connect_ret, id_photo, &photo_name_ptr))
-			printf("Received name %s\n", photo_name_ptr);
-
-		if( 1 == gallery_delete_photo(connect_ret, id_photo))
-		printf("Deleted id_photo: %d\n", id_photo);
 
 	}
 	return 0;
